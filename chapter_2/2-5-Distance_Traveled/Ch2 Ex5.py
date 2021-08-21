a9ced2ad0c71adc4721b5baf44227f0d4fcedc8b
Jhoser92 <58017514+Jhoser92@users.noreply.github.com>
2021-08-21 17:36:44 -0400

    Create Ch2 Ex5.py

---
+++ chapter_2/2-5-Distance_Traveled/Ch2 Ex5.py
@@ -0,0 +1,21 @@
+# This program will calculate
+# how far a car travels.
+
+# Set the speed.
+speed = 70
+
+# Set the times.
+time1 = 6
+time2 = 10
+time3 = 15
+
+# Calculate the distance.
+dist1 = speed * time1
+dist2 = speed * time2
+dist3 = speed * time3
+
+# Display the results.
+print('The car is going down the interstate at', speed,'mph.')
+print('In 6 hours the car would have traveled', dist1,'miles.')
+print('In 10 hours the car would have traveled', dist2,'miles.')
+print('In 15 hours the car would have traveled', dist3,'miles.')
