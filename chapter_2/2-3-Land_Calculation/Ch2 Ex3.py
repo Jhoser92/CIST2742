1792326607122683d63b4747f4a2602eb6fdd668
Jhoser92 <58017514+Jhoser92@users.noreply.github.com>
2021-08-21 18:18:56 -0400

    Update Ch2 Ex3.py

--- chapter_2/2-3-Land_Calculation/Ch2 Ex3.py
+++ chapter_2/2-3-Land_Calculation/Ch2 Ex3.py
@@ -1,4 +1,5 @@
 # 2-3 Land Calculation
+
 # One acre of land is equivalent to 43,560 square feet.
 # Write a program that asks the user to enter the total square feet in a tract
 # of land and calculates the number of acres in the tract.
