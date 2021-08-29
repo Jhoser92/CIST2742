b8f403422064b9a7f494d2299822a52d80c2480f
Jhoser92 <58017514+Jhoser92@users.noreply.github.com>
2021-08-29 05:06:35 -0400

    Update Ch3 Ex15.py

--- Chapter_3/3-15-Time_Calculator/Ch3 Ex15.py
+++ Chapter_3/3-15-Time_Calculator/Ch3 Ex15.py
@@ -25,18 +25,18 @@ seconds = int(input('Enter the number of seconds: '))
 # Calculate the time.
 if seconds >= minute and seconds < hour:
     totalm = seconds / minute
-    print('There is', format(totalm, '.3f'), 'minutes in', seconds, 'seconds')
+    print('There is', format(totalm, '.3f'), 'minutes in', seconds, 'seconds.')
 else:
     if seconds >= hour and seconds < day:
         totalm = seconds / minute
         totalh = seconds / hour
-        print('There is', format(totalh, '.3f'), 'hours in', seconds, 'seconds')
-        print('There is', format(totalm, '.3f'), 'minutes in', seconds, 'seconds')
+        print('There is', format(totalh, '.3f'), 'hours in', seconds, 'seconds.')
+        print('There is', format(totalm, '.3f'), 'minutes in', seconds, 'seconds.')
     else:
         if seconds >= day:
             totalm = seconds / minute
             totalh = seconds / hour
             totald = seconds / day
-            print('There is', format(totald, '.3f'), 'days in', seconds, 'seconds')
-            print('There is', format(totalh, '.3f'), 'hours in', seconds, 'seconds')
-            print('There is', format(totalm, '.3f'), 'minutes in', seconds, 'seconds')
+            print('There is', format(totald, '.3f'), 'days in', seconds, 'seconds.')
+            print('There is', format(totalh, '.3f'), 'hours in', seconds, 'seconds.')
+            print('There is', format(totalm, '.3f'), 'minutes in', seconds, 'seconds.')
